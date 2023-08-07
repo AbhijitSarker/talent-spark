9735883f640fe76185db26cdb9209c050770b1e2
AbhijitSarker <abhijitsarker03@gmail.com>
2023-08-08 00:07:15 +0600

    job category list section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,14 +3,14 @@ import './About.css'
 const About = () => {
     return (
         <div className='container mx-auto'>
-            <div className='background-image '>
+            <div className='background-image mb-20'>
                 <div className=' text-center py-10 lg:py-40 bg-color	container mx-auto'>
                     <h2 className='text-7xl font-bold bg-color text-white'>About Us</h2>
                     <p className='text-2xl font-medium bg-color text-white'>Connecting Careers, Empowering Lives</p>
                     <p className='px-5 md:px-52 bg-color text-white'> Welcome to TALENT SPARK, your go-to destination for all things career-related. We understand that finding the perfect job or the ideal candidate can be a daunting task. That's why we're here - to bridge the gap between talented professionals and thriving companies, creating meaningful connections that lead to fulfilling careers.</p>
                 </div>
             </div>
-            <div className="lg:flex mb-10">
+            <div className="lg:flex mb-20">
                 <div>
                     <h2 className='text-left my-5 pl-5 py-5 text-5xl underline text-blue-500 font-bold'>Our Mission</h2>
                     <p className='px-5 text-left'>At TALENT SPARK, our mission is clear and unwavering: to revolutionize the way people discover, pursue, and excel in their careers. We're dedicated to providing a seamless and efficient platform that empowers job seekers and employers alike, fostering an environment where talent meets opportunity.</p>
@@ -23,7 +23,7 @@ const About = () => {
                 </div>
 
             </div>
-            <div className='bg-service rounded-lg'>
+            <div className='bg-service rounded-lg mb-20'>
                 <h2 className='text-center my-5 py-5 text-5xl underline text-blue-500 font-bold'>Our Services</h2>
                 <div className='lg:flex justify-between'>
                     <div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,15 +2,46 @@ import React from 'react';
 import './Home.css'
 const Home = () => {
     return (
-        <div className='home-container w-auto'>
-            <div className='container mx-auto lg:grid lg:grid-cols-2 justify-between items-center'>
-                <div>
-                    <h1 className='lg:leading-relaxed text-5xl md:text-7xl font-bold p-5'>One Step <br /> CLOOSER To Your <br /><span className='text-purple-600 font-extrabold'>DREAM JOB</span></h1>
-                    <p className='p-5 text-2xl'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
-                    <button className=' mr-10 rounded-md bg-sky-500 hover:bg-purple-600 px-5 m-5 py-2 font-medium text-white'> Get Started</button>
+        <div className=' w-auto'>
+            <div className='home-container'>
+                <div className='container mx-auto lg:grid lg:grid-cols-2 justify-between items-center'>
+                    <div>
+                        <h1 className='lg:leading-relaxed text-5xl md:text-7xl font-bold p-5'>One Step <br /> CLOOSER To Your <br /><span className='text-purple-600 font-extrabold'>DREAM JOB</span></h1>
+                        <p className='p-5 text-2xl'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                        <button className=' mr-10 rounded-md bg-sky-500 hover:bg-purple-600 px-5 m-5 py-2 font-medium text-white'> Get Started</button>
+                    </div>
+                    <div>
+                        <img className='' src="/src/assets/images/P3OLGJ1 copy 1.png" alt="" />
+                    </div>
                 </div>
-                <div>
-                    <img className='float-right' src="/src/assets/images/P3OLGJ1 copy 1.png" alt="" />
+
+            </div>
+
+            <div className='container mx-auto my-20'>
+                <h3 className='text-5xl text-black mb-5 font-bold text-center'>Job Category List</h3>
+                <p className='text-center text-xl mb-16'> Explore thousands of job opportunities with all the information you need. Its your future</p>
+                <div className='grid grid-cols-4 gap-5 '>
+                    <div className='home-container p-10 w-80 h-60 rounded-md hover:bg-blue-100'>
+                        <img className='w-20 bg-purple-100 p-2 rounded-md' src="/src/assets/Icons/accounts 1.png" alt="" />
+                        <h4 className='text-2xl font-medium my-4'>Account & Finance</h4>
+                        <p className='text-gray-500'>300 Jobs Available</p>
+                    </div>
+                    <div className='home-container p-10 w-80 h-60 rounded-md hover:bg-blue-100'>
+                        <img className='w-20 bg-purple-100 p-2 rounded-md' src="/src/assets/Icons/business 1.png" alt="" />
+                        <h4 className='text-2xl font-medium my-4'>Creative Design</h4>
+                        <p className='text-gray-500'>100+ Jobs Available</p>
+                    </div>
+                    <div className='home-container p-10 w-80 h-60 rounded-md hover:bg-blue-100'>
+                        <img className='w-20 bg-purple-100 p-2 rounded-md' src="/src/assets/Icons/social-media 1.png" alt="" />
+                        <h4 className='text-2xl font-medium my-4'>Marketing & Sales</h4>
+                        <p className='text-gray-500'>150 Jobs Available</p>
+                    </div>
+                    <div className='home-container p-10 w-80 h-60 rounded-md hover:bg-blue-100'>
+                        <img className='w-20 bg-purple-100 p-2 rounded-md' src="/src/assets/Icons/chip 1.png" alt="" />
+                        <h4 className='text-2xl font-medium my-4'>Engineering Job</h4>
+                        <p className='text-gray-500'>224 Jobs Available</p>
+                    </div>
+
                 </div>
             </div>
         </div>
